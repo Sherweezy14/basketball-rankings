62fc40c5b92066f87a4169b776d19b775a5bd618
Sherwyn Cooper <newenglandballas@gmail.com>
2026-06-19 19:34:31 -0400

    updated mobile view of nav bar made text larger and nav bar itself

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -9,22 +9,22 @@ function Navbar() {
 
   return (
     <nav className="bg-white border-b border-slate-200 sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
+      <div className="h-48 lg:max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
         <Link to="/" className="lg:hidden flex items-center gap-3">
-          <img src="/img/logo1.png" alt="HoopScout" className="h-18 w-auto" />
+          <img src="/img/logo1.png" alt="HoopScout" className="h-44 lg:h-18 w-auto" />
           <div>
-            <h1 className="font-black text-xl text-slate-900">
+            <h1 className="hidden lg:font-black text-xl text-slate-900">
               HoopScout
             </h1>
 
-            <p className="text-xs text-slate-500">
+            <p className="hidden lg: text-xs text-slate-500">
               Recruiting Platform
             </p>
           </div>
         </Link>
 
-        <div className="hidden md:flex items-center gap-8">
-          <Link to="/" className="font-semibold text-slate-700 hover:text-purple-700">Rankings</Link>
+        <div className="hidden lg:flex  items-center gap-8">
+          <Link to="/" className=" font-semibold text-slate-700 hover:text-purple-700">Rankings</Link>
           <Link to="/articles" className="font-semibold text-slate-700 hover:text-purple-700">Articles</Link>
           <Link to="/" className="font-semibold text-slate-700 hover:text-purple-700">Events</Link>
           <SearchPlayers className="border-1"/>
@@ -35,14 +35,14 @@ function Navbar() {
 
         <button
           onClick={() => setMenuOpen(!menuOpen)}
-          className="md:hidden text-3xl font-bold text-slate-900"
+          className="text-3xl lg:hidden text-6xl font-bold text-slate-900"
         >
           ☰
         </button>
       </div>
 
       {menuOpen && (
-        <div className="md:hidden bg-white border-t border-slate-200 px-6 py-4 space-y-4">
+        <div className="lg:hidden bg-white border-t border-slate-200 text-4xl px-6 py-4 space-y-4">
           <Link onClick={() => setMenuOpen(false)} to="/" className="block font-semibold text-slate-700">
             Rankings
           </Link>
@@ -54,6 +54,11 @@ function Navbar() {
           <Link onClick={() => setMenuOpen(false)} to="/" className="block font-semibold text-slate-700">
             Events
           </Link>
+
+          <SearchPlayers className="border-1 "/>
+
+          <UserLogin className=""/>
+          {token && (<Link to="/createplayer" className="bg-purple-700 text-white font-bold px-5 py-2.5 rounded-xl">+ Add Player</Link>)}
 
         </div>
       )}
