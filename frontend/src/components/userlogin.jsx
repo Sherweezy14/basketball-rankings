755e94a9725033b9e13887226d994e9f3a995108
Sherwyn Cooper <newenglandballas@gmail.com>
2026-06-22 19:02:39 -0400

    added regex for input on login form displays error messages until its correct. made fields required and shows alert message if user name or password is incorrect

--- frontend/src/components/userlogin.jsx
+++ frontend/src/components/userlogin.jsx
@@ -1,35 +1,42 @@
 import { loginUser } from "../util/playerApi"
 import { useState } from "react"
 import {authUser} from "../context/tokencontext"
+import { useNavigate } from "react-router-dom"
 
 function UserLogin(){
+    const nav = useNavigate()
     const[data,setData] = useState({})
-    const[errors,setErrors] = useState(null)
+    const[errors,setErrors] = useState({})
     const{token, userLoggedIn, logIn, logOut}  = authUser()
     const regexTest = {email:/^[^\s@]+@[^\s@]+\.[^\s@]+$/,
                    password:/^(?=.*[A-Za-z])(?=.*\d).{8,}$/}
 
     function change(event){
-        // Check each field input against regex for err message remove err if field input is empty string
-        regexTest[event.target.name].test(event.target.value)? setErrors({...errors,[event.target.name]:false}): setErrors({...errors,[event.target.name]:true}) 
-        if(event.target.value === ""){
-            const {[event.target.name]: value, ...leftOver} = errors
-            setErrors(leftOver)
-
-        } 
+        // Check each field input against regex 
+        // if it matches remove error if it fails add err
+        if(regexTest[event.target.name].test(event.target.value) || event.target.value === ""){
+            const {[event.target.name]:value,...left} = errors
+            setErrors(left)
+        }else{
+            setErrors({...errors,[event.target.name]:true}) 
+        }
 
 
         setData({...data,[event.target.name]:event.target.value})
+   
 
     }
 
     async function logInUserInput(event){
         event.preventDefault();
         const user = await loginUser(data)
-        
         if(user.token){
             logIn(user)
-        }    
+            nav("/")
+        }else{
+            alert("User Name or Password Incorrect")
+        } 
+
     }
 
     function logOutUser(event){
@@ -38,15 +45,15 @@ function UserLogin(){
     }
 
     return (!token ? ( <>
-        <form className="flex flex-col items-center gap-2 " action="Submit">
-            <input name="email" className="w-full rounded-xl border border-slate-300 bg-slate-50 px-4 py-2" placeholder="Email" onChange={change} type="text" />
+        <form className="flex flex-col items-center gap-2 " action="Submit" onSubmit={logInUserInput}>
+            <input required name="email" className="w-full rounded-xl border border-slate-300 bg-slate-50 px-4 py-2" placeholder="Email" onChange={change} type="text" />
             {errors?.email && <p className="text-red-600"> Must be an email.</p>}
-            <input name="password" className="w-full rounded-xl border border-slate-300 bg-slate-50 px-4 py-2" placeholder="Password" onChange={change} type="password" />
+            <input required name="password" className="w-full rounded-xl border border-slate-300 bg-slate-50 px-4 py-2" placeholder="Password" onChange={change} type="password" />
             {errors?.password && <p className="text-red-600"> Password must be at least 8 characters and include a letter and a number.</p>}
-            <button className="bg-purple-700 text-white font-bold px-4 py-2 rounded-xl" onClick={logInUserInput}> Login </button>
+            <button disabled={Object.keys(errors).length !== 0} className="bg-purple-700 text-white font-bold px-4 py-2 rounded-xl disabled:bg-slate-300 disabled:text-slate-500 disabled:hover:bg-slate-300 disabled:cursor-not-allowed" type="Submit" > Login </button>
         </form>
-    </> ): (<div className="flex items-center gap-8">
-            <p className="hidden lg:block"> {"Hello " + userLoggedIn.name.split(" ")[0]}</p>
+    </> ): (<div className="flex flex-col items-center gap-2">
+            <p className="hidden text-2xl lg:block"> {"Hello " + userLoggedIn.name.split(" ")[0]}</p>
             <p className="text-red-500 mt-5 text-1xl lg:hidden" onClick={logOutUser}> Sign Out</p>
             <button className="hidden lg:block bg-red-700 text-white font-bold px-3 py-1.5 rounded-xl" onClick={logOutUser}> LogOut </button>
            </div>)
--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -1,7 +1,7 @@
 import UserLogin from "../components/userlogin"
-import { useNavigate,Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 export default function LogInPage(){
-    const navigate = useNavigate();
+    
 
 
 
