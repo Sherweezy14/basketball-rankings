8f683fd07fd0450c4c979084e1d7d68b2e9cb5d0
Sherwyn Cooper <newenglandballas@gmail.com>
2026-06-24 17:03:37 -0400

    bug was returning unlogged in guess back to home page instead of login in page fixed

--- frontend/src/components/protectedRoutes.jsx
+++ frontend/src/components/protectedRoutes.jsx
@@ -5,8 +5,9 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children, permission }) {
   const { userLoggedIn } = authUser();
+
   if (!userLoggedIn) {
-    <Navigate to="/login" replace />;
+    return <Navigate to="/login" replace />;
   }
   if (hasPermission(userLoggedIn?.role, permission)) {
     return children;
